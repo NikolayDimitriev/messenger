769b2293d3edf10a7df5d6187c9fa07317697194
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-01-16 23:51:27 +0300

    create auth page. added form and inputs components

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,6 +1,23 @@
-import tpl from './tpl.hbs';
-import './style.css';
+import tpl from "./tpl.hbs";
+import "./style.scss";
+import button from "../../components/button";
+import link from "../../components/link";
+import form from "../../components/form";
+import { logIn } from "../../../data";
 
-export default (props = {}) => {
-    return tpl(props)
+export default () => {
+  return tpl({
+    form: form({
+      data: logIn,
+      button: button({
+        value: "Авторизоваться",
+        className: "main-btn",
+      }),
+      link: link({
+        href: "/registration",
+        value: "Нет аккаунта?",
+        className: "form-link",
+      }),
+    }),
+  });
 };
