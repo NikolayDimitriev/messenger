50cf3fa14330c92e7b998054367392f73ab53697
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-01-17 21:12:14 +0300

    created error pages

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,6 +1,17 @@
 import tpl from './tpl.hbs';
-import './style.css';
+import error from '../../components/error';
+import link from '../../components/link';
 
-export default (props = {}) => {
-    return tpl(props)
+export default () => {
+    return tpl({
+			error: error({
+				errorCode: "400",
+				errorText: "Не туда попали",
+				link: link({
+					href: "/chats",
+					value: "Назад к чатам",
+					className: "form-link",
+				}),
+			})
+    });
 };
--- src/pages/500/index.js
+++ src/pages/500/index.js
@@ -1,6 +1,17 @@
-import tpl from './tpl.hbs';
-import './style.css';
+import tpl from "./tpl.hbs";
+import error from "../../components/error";
+import link from "../../components/link";
 
-export default (props = {}) => {
-    return tpl(props)
+export default () => {
+  return tpl({
+    error: error({
+      errorCode: "500",
+      errorText: "Мы уже фиксим",
+      link: link({
+        href: "/chats",
+        value: "Назад к чатам",
+        className: "form-link",
+      }),
+    }),
+  });
 };
