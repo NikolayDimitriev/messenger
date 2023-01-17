8ca5366d44bad7527db62e35b6da06809366201d
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-01-17 21:10:13 +0300

    adding basic routing

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import profilePage from "./pages/profile";
 import chatsPage from "./pages/chats";
 import page404 from "./pages/404";
 import page500 from "./pages/500";
+import navPage from "./pages/navigation";
 
 import tpl from "./index.hbs";
 
 const app = document.getElementById("root");
 
 const routes = [
-  { path: "/", page: authPage() },
+  { path: "/", page: navPage() },
+  { path: "/auth", page: authPage() },
   { path: "/registration", page: registerPage() },
   { path: "/profile", page: profilePage() },
   { path: "/chats", page: chatsPage() },
@@ -18,9 +20,11 @@ const routes = [
   { path: "/500", page: page500() },
 ];
 
-function router(event) {
-  const url = window.location.hash.slice(1) || "/";
-  const { page } = routes.find(({ path }) => path === url);
+function router() {
+  const url = window.location.pathname;
+  const { page } = routes.find(({ path }) => path === url) || {
+    page: page404(),
+  };
 
   app.innerHTML = tpl({
     page,
@@ -28,4 +32,4 @@ function router(event) {
 }
 
 window.addEventListener("load", router);
-window.addEventListener("hashchange", router);
+
