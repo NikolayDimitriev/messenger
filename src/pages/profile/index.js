be77fd45bce236eb1a9cd2e03b839d8532cb02bf
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-01-17 23:41:16 +0300

    create profile page layout

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,6 +1,14 @@
-import tpl from './tpl.hbs';
-import './style.css';
+import tpl from "./tpl.hbs";
+import avatar from "../../../static/avatar.svg";
+import profileInputs from "../../components/profileInputs";
+import { user } from "../../../data";
+import "./style.scss";
 
-export default (props = {}) => {
-    return tpl(props)
+export default () => {
+  return tpl({
+    avatar: avatar,
+		name: user.shortFormat.first_name,
+    fields: profileInputs({ data: user.inputs }),
+    passwords: profileInputs({ data: user.passwords }),
+  });
 };
